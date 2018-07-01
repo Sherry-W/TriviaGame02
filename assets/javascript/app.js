a0bd69642c20080c9087613da274103c5106eb41
Sherry-W <sherry.wu1219@gmail.com>
2018-06-30 23:34:16 -0700

    css and more questions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+
+// trivia questions
 // https://opentdb.com/api_config.php
 // https://opentdb.com/api.php?amount=10&category=27
 
@@ -48,6 +50,66 @@
 		  answers: ["Japanese sea lion", "Komodo dragon", "Tasmanian tiger", "Saudi gazelle"],
 		  correct_answers: "Komodo dragon",
 		  image:"assets/images/q_five.jpg"
+		},
+		{
+		  question: "Cashmere is the wool from which kind of animal?",
+		  answers: ["Sheep", "Camel", "Goat", "Llama"],
+		  correct_answers: "Goat",
+			image:"assets/images/q_six.jpg"
+		},
+		{
+		  question: "What is Grumpy Cat's real name?",
+		  answers: ["Sauce", "Tardar Sauce", "Minnie", "Broccoli"],
+		  correct_answers: "Tardar Sauce",
+		  image:"assets/images/q_seven.jpg"
+		},
+		{
+		  question: "What is the fastest animal?",
+		  answers: ["Peregrine Falcon", "Golden Eagle", "Cheetah", "Horsefly"],
+		  correct_answers: "Peregrine Falcon",
+		  image:"assets/images/q_eight.jpg"
+		},
+		{
+		  question: "What is the scientific name for the 'Polar Bear'?",
+		  answers: ["Polar Bear", "Ursus Maritimus", "Ursus Spelaeus", "Ursus Arctos"],
+		  correct_answers: "Ursus Maritimus",
+	 	  image:"assets/images/q_nine.jpg"
+		},
+		{
+		  question: "Which animal was part of an Russian domestication experiment in 1959?",
+		  answers: ["Pigeons", "Foxes", "Bears", "Alligators"],
+		  correct_answers: "Foxes",
+	 	  image:"assets/images/q_ten.png"
+		},
+		{
+		  question: "What dog bread is one of the oldest breeds of dog and has flourished since before 400 BCE.",
+		  answers: ["Bulldogs", "Boxers", "Pugs", "Chihuahua"],
+		  correct_answers: "Pugs",
+		  image:"assets/images/q_eleven.jpg"
+		},
+		{
+		  question: "Hippocampus is the Latin name for which marine creature?",
+		  answers: ["Dolphin", "Whale", "Octopus", "Seahorse"],
+		  correct_answers: "Seahorse",
+	 	  image:"assets/images/q_12.jpg"
+		},
+		{
+		  question: "What is the scientific name of the cheetah?",
+		  answers: ["Acinonyx jubatus", "Panthera onca", "Lynx rufus", "Felis catus"],
+		  correct_answers: "Acinonyx jubatus",
+	   	  image:"assets/images/q_13.jpg"
+		},
+		{
+		  question: "What was the name of the Ethiopian Wolf before they knew it was related to wolves?",
+		  answers: ["Ethiopian Coyote", "Amharic Fox", "Canis Simiensis", "Simien Jackel"],
+		  correct_answers: "Simien Jackel",
+	 	  image:"assets/images/q_14.jpg"
+		},
+		{
+		  question: "Which species of Brown Bear is not extinct?",
+		  answers: ["California Grizzly Bear", "Syrian Brown Bear", "Atlas Bear", "Mexican Grizzly Bear"],
+		  correct_answers: "Syrian Brown Bear",
+	 	  image:"assets/images/q_15.jpg"
 		}];
 
 // displays
@@ -87,46 +149,45 @@
 		}
 	}
 
+// stop timer when question answered/clicked
 	function stop() {
 		clearInterval(countdown);
 	}
 
-	function resetTimer() {
-		clearInterval(countdown);
-		timer = 30;
-	}
-
 //loop through to generate the questions and answer
-	quesIndex = 0;
-
-	function showing() {
+	function showing() {	
 		if (quesIndex == questions.length) {
 			score();
 		}
 
+	//append question
 		ques = $("<h3>").text(questions[quesIndex].question);
 		$('#questions').append(ques);
 
+	//randomize the choices and append them
 		choice = questions[quesIndex].answers;
 		choice = choice.sort(function(a, b){return 0.5 - Math.random()});
 
 		for (var j = 0; j < choice.length; j++) { 
 
-				multChoice = $('<p>').html(choice[j]);
-				// console.log(choice[j]);
-				$('#questions').append(multChoice);
-			}
+			multChoice = $('<p>').html(choice[j]);
+			// console.log(choice[j]);
+			$('#questions').append(multChoice);
+		}
 	}
 
+//this helps to show the next question
 	function next() {
 		quesIndex++;
 		$('#questions').text("");
 
 		showing();
 		timer = 30;
+		$('#timer').text("Time Remaining: " + timer + " Seconds");
 		countdown = setInterval(start, 1000);
 	}
 
+//when user clicks on an answer (the p tag)
 	$(document).on('click', 'p', function() {
 		stop();
 		$('#timer').text("Time Remaining: " + timer + " Seconds");
@@ -158,7 +219,6 @@
 		$('#questions').append('<img src="' + questions[quesIndex].image + '" />');
 		setTimeout(next, 3 * 1000);
 	}
-	
 
 //	count the scores
 	function score() {
@@ -167,11 +227,8 @@
 		clearInterval(countdown);
 		timer = 0;
 		
-		
 		console.log ("correct: " + correct, "wrong: " + wrong, "unanswered: " + unanswered);
 
-		// $('#timer').hide();
-		// $('#questions').hide();
 		var msg = $('<h4>').text('All done, heres how you did!');
 		$('#timer').append(msg);
 
